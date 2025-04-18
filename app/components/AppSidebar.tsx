785225a7a7f6ab5b26824684558bc18986182e1d
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-04-17 20:49:14 -0500

    style: font

--- app/components/AppSidebar.tsx
+++ app/components/AppSidebar.tsx
@@ -35,15 +35,15 @@ export function AppSidebar() {
   return (
     <Sidebar variant="inset"  collapsible="icon">
       <SidebarContent>
-        <SidebarGroup className="h-full  flex flex-col gap-4">
+        <SidebarGroup className="h-full  flex flex-col gap-8">
           <SidebarGroupLabel className="text-1xl text-background font-bold">Budget Brain 🧠</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu className="flex flex-col gap-2">
+            <SidebarMenu className="flex flex-col gap-4">
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton className="text-xl font-medium" asChild>
                     <Link href={item.url}>
-                      <item.icon />
+                      <item.icon  />
                       <span>{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
@@ -54,17 +54,17 @@ export function AppSidebar() {
         </SidebarGroup>
         <SidebarGroup>
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="flex flex-col gap-4">
             <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton className="text-xl" asChild>
                     <Link href="/settings">
                       <Settings />
                       <span>Profile & Settings</span>
                     </Link>
                   </SidebarMenuButton>
             </SidebarMenuItem>
-            <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
+            <SidebarMenuItem >
+                  <SidebarMenuButton className="text-xl" asChild>
                     <a href="#">
                       <LogOut />
                       <span>Logout</span>
