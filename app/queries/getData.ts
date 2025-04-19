5cb699595b32345f85e6112e694cbb6ceec78585
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-04-19 17:28:32 -0500

    remove old data

--- app/queries/getData.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/utils/supabase/server"
-
-export const getData = async () => {
-  const supabase = await createClient()
-  const { data } = await supabase.from('instruments').select('*')
-  console.log("🚀 ~ getData ~ data:", data)
-  return data
-}
-
