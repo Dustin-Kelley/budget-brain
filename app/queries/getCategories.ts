0591497a7138ed49a522a7150386384a32952271
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-04-30 08:56:12 -0500

    chore: clean up

--- app/queries/getCategories.ts
+++ app/queries/getCategories.ts
@@ -1,22 +1,23 @@
-import { createClient } from "@/utils/supabase/server";
-import { getCurrentUser } from "./getCurrentUser";
-import { redirect } from "next/navigation";
-export async function getCategories() {
+import { createClient } from '@/utils/supabase/server';
+import { getCurrentUser } from './getCurrentUser';
+import { redirect } from 'next/navigation';
+import { cache } from 'react';
+export const getCategories = cache(async () => {
   const supabase = await createClient();
   const { currentUser } = await getCurrentUser();
 
   if (!currentUser) {
-    redirect("/login");
+    redirect('/login');
   }
 
   const { data, error } = await supabase
-    .from("categories")
-    .select("*, line_items(*)")
-    .eq("household_id", currentUser.household_id);
+    .from('categories')
+    .select('*, line_items(*)')
+    .eq('household_id', currentUser.household_id);
 
   if (error) {
     console.error(error);
   }
 
   return { data, error };
-}
+});
--- app/queries/getTotalIncome.ts
+++ app/queries/getTotalIncome.ts
@@ -1,27 +1,28 @@
-import { createClient } from "@/utils/supabase/server";
-import { getCurrentUser } from "./getCurrentUser";
-import { redirect } from "next/navigation";
+import { createClient } from '@/utils/supabase/server';
+import { getCurrentUser } from './getCurrentUser';
+import { redirect } from 'next/navigation';
+import { cache } from 'react';
 
-export async function getTotalIncome() {
+export const getTotalIncome = cache(async () => {
   const supabase = await createClient();
   const { currentUser } = await getCurrentUser();
 
   if (!currentUser) {
-    redirect("/login");
+    redirect('/login');
   }
 
   const { data, error } = await supabase
-    .from("income")
-    .select("*")
-    .eq("household_id", currentUser.household_id)
-    .eq("created_by", currentUser.id);
+    .from('income')
+    .select('*')
+    .eq('household_id', currentUser.household_id)
+    .eq('created_by', currentUser.id);
 
   if (error) {
-   console.error(error);
+    console.error(error);
   }
 
   return {
     data,
     error,
   };
-}
+});
