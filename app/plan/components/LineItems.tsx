c42d54c0fc8db21f5a31178e9054375294c5709c
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-05-09 09:08:22 -0500

    fix: update Lineitem type

--- app/plan/components/LineItems.tsx
+++ app/plan/components/LineItems.tsx
@@ -10,21 +10,9 @@ import {
 } from '@/components/ui/popover';
 import { Button } from '@/components/ui/button';
 import { EditLineItemForm } from './EditLineItemForm';
+import { LineItem } from '@/types/types';
 
-type LineItems = {
-    category_id: string;
-    created_at: string;
-    created_by: string | null;
-    id: string;
-    month: number | null;
-    name: string | null;
-    planned_amount: number | null;
-    spent_amount: number | null;
-    updated_at: string | null;
-    year: number | null;
-}
-
-export const LineItems = ({lineItems}: {lineItems: LineItems} ) => {
+export const LineItems = ({ lineItems }: { lineItems: LineItem }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -32,21 +20,23 @@ export const LineItems = ({lineItems}: {lineItems: LineItems} ) => {
       key={lineItems.id}
       className='flex items-center justify-between text-sm'
     >
-      <span className='text-muted-foreground'>
-        {lineItems.name}
-      </span>
+      <span className='text-muted-foreground'>{lineItems.name}</span>
       <div className='flex items-center gap-2'>
-        <span className='font-medium'>
-          ${lineItems.planned_amount}
-        </span>
-        <Popover open={isOpen} onOpenChange={setIsOpen}>
+        <span className='font-medium'>${lineItems.planned_amount}</span>
+        <Popover
+          open={isOpen}
+          onOpenChange={setIsOpen}
+        >
           <PopoverTrigger asChild>
-            <Button variant="ghost" size="icon" >
-              <MoreHorizontal className="h-4 w-4" />
-              <span className="sr-only">Open menu</span>
+            <Button
+              variant='ghost'
+              size='icon'
+            >
+              <MoreHorizontal className='h-4 w-4' />
+              <span className='sr-only'>Open menu</span>
             </Button>
           </PopoverTrigger>
-          <PopoverContent className='flex gap-2 flex-col' >
+          <PopoverContent className='flex gap-2 flex-col'>
             <DeleteItemButton lineItemId={lineItems.id} />
             <EditLineItemForm lineItem={lineItems} />
           </PopoverContent>
