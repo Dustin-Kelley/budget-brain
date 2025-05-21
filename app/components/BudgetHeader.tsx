06486a23ae63afbfc63641ec66ef3e7a58ac0efa
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-05-20 20:21:14 -0500

    style: padding

--- app/components/BudgetHeader.tsx
+++ app/components/BudgetHeader.tsx
@@ -9,7 +9,7 @@ export async function BudgetHeader({ month }: { month: string | undefined }) {
   const { categories } = await getCategories({ date: month });
 
   return (
-    <div className='flex md:flex-row gap-2 items-end justify-between'>
+    <div className='flex md:flex-row gap-2 pb-4 items-end justify-between'>
       <div>
         <h1 className='text-3xl font-bold text-secondary tracking-tight'>
           {parsedMonth}
