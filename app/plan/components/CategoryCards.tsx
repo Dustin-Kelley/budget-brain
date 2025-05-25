66fafd5860d2a373f3aaba81df4ac20140303416
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-05-25 11:56:17 -0500

    fix: nan

--- app/plan/components/CategoryCards.tsx
+++ app/plan/components/CategoryCards.tsx
@@ -15,68 +15,74 @@ import { EditCategories } from './EditCategories';
 import { Separator } from '@/components/ui/separator';
 
 export async function CategoryCards({ month }: { month: string | undefined }) {
-
   const { totalIncome } = await getTotalIncomePerMonth({ date: month });
   const { categories } = await getCategories({ date: month });
- 
+
   return (
-    <div className="columns-1 md:columns-2 lg:columns-2 space-y-4">
-      {categories?.map((category) => {
-        const categoryLineItems = category.line_items?.filter(
-          (sub) => sub.category_id === category.id
-        );
-        return (
-          <Card
-            className="mb-4 break-inside-avoid flex flex-col justify-between"
-            key={category.id}
-          >
-            <CardHeader className='pb-2'>
-              <div className='flex items-center justify-between'>
-                <CardTitle className='font-bold text-lg'>
-                  {category.name}
-                </CardTitle>
-                <EditCategories categoryId={category.id} categoryName={category.name} />
-              </div>
-            </CardHeader>
-            <CardContent className='flex flex-col gap-2'>
-              <Separator  />
-              {categoryLineItems.map((lineItems) => (
-                <LineItems
-                  key={lineItems.id}
-                  lineItems={lineItems}
+    <div className='flex  flex-col gap-4'>
+      <div className='columns-1 md:columns-2 lg:columns-2 space-y-4'>
+        {categories?.map((category) => {
+          const categoryLineItems = category.line_items?.filter(
+            (sub) => sub.category_id === category.id
+          );
+          return (
+            <Card
+              className='mb-4 break-inside-avoid flex flex-col justify-between'
+              key={category.id}
+            >
+              <CardHeader className='pb-2'>
+                <div className='flex items-center justify-between'>
+                  <CardTitle className='font-bold text-lg'>
+                    {category.name}
+                  </CardTitle>
+                  <EditCategories
+                    categoryId={category.id}
+                    categoryName={category.name}
+                  />
+                </div>
+              </CardHeader>
+              <CardContent className='flex flex-col gap-2'>
+                <Separator />
+                {categoryLineItems.map((lineItems) => (
+                  <LineItems
+                    key={lineItems.id}
+                    lineItems={lineItems}
+                  />
+                ))}
+                <AddNewItemForm
+                  categoryName={category.name}
+                  categoryId={category.id}
+                  month={month}
                 />
-              ))}
-              <AddNewItemForm
-                categoryName={category.name}
-                categoryId={category.id}
-                month={month}
-              />
-            </CardContent>
-            <CardFooter className='pt-0'>
-              <div className='flex w-full justify-between text-xs text-muted-foreground'>
-                <span>
-                  Total Category Budget: $
-                  {category.line_items?.reduce(
-                    (acc, item) => acc + (item?.planned_amount || 0),
-                    0
-                  )}
-                </span>
-                <span>
-                  {Math.round(
-                    (category.line_items?.reduce(
-                      (acc, item) => acc + (item?.planned_amount || 0),
+              </CardContent>
+              <CardFooter className='pt-0'>
+                <div className='flex w-full justify-between text-xs text-muted-foreground'>
+                  <span>
+                    Total Category Budget: $
+                    {category.line_items?.reduce(
+                      (acc, item) => acc + (item?.planned_amount ?? 0),
                       0
-                    ) /
-                      totalIncome) *
-                      100
-                  )}
-                  %
-                </span>
-              </div>
-            </CardFooter>
-          </Card>
-        );
-      })}
+                    )}
+                  </span>
+                  <span>
+                    {totalIncome
+                      ? Math.round(
+                          (category.line_items?.reduce(
+                            (acc, item) => acc + (item?.planned_amount ?? 0),
+                            0
+                          ) /
+                            totalIncome) *
+                            100
+                        )
+                      : 0}
+                    %
+                  </span>
+                </div>
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </div>
       <AddNewCategoryForm month={month} />
     </div>
   );
