a09885705b448c6bbef9c37c480bf3216f4aec70
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-05-25 11:49:39 -0500

    feat: add alert

--- app/components/ResetBudgetButton.tsx
+++ app/components/ResetBudgetButton.tsx
@@ -1,9 +1,20 @@
 'use client';
 
-import { Button } from "@/components/ui/button";
-import { resetBudget } from "../mutations/resetBudget";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { resetBudget } from '../mutations/resetBudget';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
 export const ResetBudgetButton = ({ month }: { month: string | undefined }) => {
   const router = useRouter();
@@ -16,6 +27,31 @@ export const ResetBudgetButton = ({ month }: { month: string | undefined }) => {
     router.refresh();
   };
 
-  return <Button variant='outline' onClick={handleResetBudget}>Reset Budget</Button>;
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant='destructive'>Reset Budget</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete this
+            month&apos;s budget and all of its data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className='cursor-pointer'>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            className='cursor-pointer bg-destructive text-white hover:bg-destructive/90'
+            onClick={() => handleResetBudget()}
+          >
+            Reset
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
-
