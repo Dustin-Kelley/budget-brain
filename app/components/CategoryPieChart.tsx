fc62eca81c8e0e6067439c5b792eaa4716d26d16
Dustin Kelley <Kelleydustin2018@gmail.com>
2025-05-25 11:10:24 -0500

    clean up

--- app/components/CategoryPieChart.tsx
+++ app/components/CategoryPieChart.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { TrendingUp } from 'lucide-react';
 import { Label, Pie, PieChart, Cell } from 'recharts';
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -51,8 +49,8 @@ export function CategoryPieChart({
   return (
     <Card className='flex flex-col'>
       <CardHeader className='items-center pb-0'>
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Budget Breakdown</CardTitle>
+        <CardDescription>Showing total planned for this month</CardDescription>
       </CardHeader>
       <CardContent className='flex-1 pb-0'>
         <ChartContainer
@@ -121,14 +119,6 @@ export function CategoryPieChart({
           </PieChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className='flex-col gap-2 text-sm'>
-        <div className='flex items-center gap-2 font-medium leading-none'>
-          Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
-        </div>
-        <div className='leading-none text-muted-foreground'>
-          Showing total planned for the last 6 months
-        </div>
-      </CardFooter>
     </Card>
   );
 }
